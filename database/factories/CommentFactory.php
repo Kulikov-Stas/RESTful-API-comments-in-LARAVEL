05354e48de4e6e627e7735045da267830c90d17f
Kulikov-stas <stasus2002@ukr.net>
2019-08-16 23:31:54 +0300

    phpunit

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,10 +3,11 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Comment;
-use Faker\Generator as Faker;
 
-$factory->define(Comment::class, function (Faker $faker) {
+$factory->define(Comment::class, function (Faker\Generator $faker) {
     return [
-        //
+        'name' => $faker->sentence,
+        'email' => $faker->email,
+        'text' => $faker->paragraph,
     ];
 });
---
+++ tests/Feature/CommentsTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Comment;
+use Tests\TestCase;
+
+class CommentsTest extends TestCase
+{
+
+    public function testsCommentsAreCreatedCorrectly()
+    {
+        $response = $this->json('POST',
+            '/api/comments/', [
+                'name' => 'Luk',
+                'email' => 'dark@will.be',
+                'text' => 'this is test'
+            ]
+        );
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => 'Luk', 'email' => 'dark@will.be', 'text' => 'this is test']);
+    }
+
+    public function testsCommentsAreUpdatedCorrectly()
+    {
+        $comment = factory(Comment::class)->create([
+            'name' => 'Luk',
+            'email' => 'dark@will.be',
+            'text' => 'this is test'
+        ]);
+
+        $payload = [
+            'name' => 'Lorem',
+            'email' => 'Ipsum',
+            'text' => 'Lorem Ipsum'
+        ];
+
+        $response = $this->json('PUT', '/api/articles/' . $comment->id, $payload)
+            ->assertStatus(200)
+            ->assertJson([
+                'id' => 1,
+                'name' => 'Lorem',
+                'email' => 'Ipsum',
+                'text' => 'Lorem Ipsum'
+            ]);
+    }
+
+    public function testsArtilcesAreDeletedCorrectly()
+    {
+        $comment = factory(Comment::class)->create([
+            'name' => 'Luk',
+            'email' => 'dark@will.be',
+            'text' => 'this is test'
+        ]);
+
+        $this->json('DELETE', '/api/comments/' . $comment->id, [])
+            ->assertStatus(204);
+    }
+
+    public function testCommentsAreListedCorrectly()
+    {
+        factory(Comment::class)->create([
+            'name' => 'Luk',
+            'email' => 'dark@will.be',
+            'text' => 'this is test'
+        ]);
+
+        factory(Comment::class)->create([
+            'name' => 'Yoda',
+            'email' => 'light@always.be',
+            'text' => 'this is test'
+        ]);
+
+        $response = $this->json('GET', '/api/comments', [])
+            ->assertStatus(200)
+            ->assertJson([
+                [   'name' => 'Luk',
+                    'email' => 'dark@will.be',
+                    'text' => 'this is test'
+                ],
+                [   'name' => 'Yoda',
+                    'email' => 'light@always.be',
+                    'text' => 'this is test'
+                ]
+            ])
+            ->assertJsonStructure([
+                '*' => ['id', 'name', 'email', 'text', 'parent_id', 'created_at', 'updated_at'],
+            ]);
+    }
+
+}
